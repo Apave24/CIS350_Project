c79fef40ad4b360fa68318b8077946da06380684
kielstreak <kielev@mail.gvsu.edu>
2017-07-17 12:04:42 -0400

    updated MainGUI with ActionListeners

--- src/edu/gvsu/cis350/MainGUI.java
+++ src/edu/gvsu/cis350/MainGUI.java
@@ -3,10 +3,13 @@ package edu.gvsu.cis350;
 import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbMovies;
 import info.movito.themoviedbapi.TmdbPeople;
+import info.movito.themoviedbapi.model.Artwork;
 import info.movito.themoviedbapi.model.people.Person;
 
 import java.awt.EventQueue;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JSplitPane;
@@ -22,9 +25,14 @@ import java.awt.event.ActionEvent;
 import javax.swing.JTextPane;
 
 import java.awt.Font;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import javax.swing.UIManager;
+import javax.swing.JLabel;
 
 /**
  * @author evankiel
@@ -39,6 +47,35 @@ public class MainGUI {
 	 */
 	private JFrame frame;
 	
+	/**
+	 * 
+	 */
+	private int correctAnswer;
+	/**
+	 * 
+	 */
+	private Person primaryActor;
+	/**
+	 * 
+	 */
+	private Person relatedActor;
+	/**
+	 * 
+	 */
+	private List<Person> actors = new ArrayList<Person>();
+	/**
+	 * 
+	 */
+	private List<Artwork> primaryActorImage;
+	
+	private JButton btnOne;
+	private JButton btnTwo;
+	private JButton btnThree;
+	private JButton btnFour;
+	private JButton btnFive;
+	private ImageIcon icon;
+	private JTextPane textPane3;
+	private JLabel lblNewLabel;
 	
 	/**
 	 * @param args String[]
@@ -58,160 +95,226 @@ public class MainGUI {
 
 	/**
 	 * Create the application.
+	 * @throws MalformedURLException 
 	 */
-	public MainGUI() {
-			initialize();
+	public MainGUI() throws MalformedURLException {
+		initialize();
 	}
-
+	
 	/**
-	 * Initialize the contents of the frame.
+	 * 
 	 */
-	private void initialize() {
-		
+	public void update() {
+		Random randomIndex = new Random();
+		correctAnswer = randomIndex.nextInt(4);
 		TmdbApi tmdbApi 
 			= new TmdbApi("ee5a0a6208f35c4a8010636efd3f5d9b");
 		TmdbPeople tmdbPeople = tmdbApi.getPeople();
 		TmdbMovies tmdbMovies = tmdbApi.getMovies(); 
-	
-		Person primaryActor = TmDBModel.getPrimaryActor(tmdbPeople);
-		Person relatedActor = TmDBModel.getRelatedActor(tmdbPeople,tmdbMovies, primaryActor);
-		Person actor;
+		primaryActor = TmDBModel.getPrimaryActor(tmdbPeople);
+		relatedActor = TmDBModel.getRelatedActor(
+				tmdbPeople, tmdbMovies, primaryActor);
 		
-		Random randomIndex = new Random();
-		final int correctAnswer = randomIndex.nextInt(4);
+		primaryActorImage 
+			= tmdbPeople.getPersonImages(primaryActor.getId());
+		for (int x = 0; x < 5; x++) {
+			if (correctAnswer == x) {
+				actors.add(relatedActor);
+			} else {
+				actors.add(TmDBModel.getIncorrectAnswers(
+						tmdbPeople, primaryActor));
+			}
+		}
+	}
+	
+	/**
+	 * @throws MalformedURLException a
+	 */
+	public void updateGUI() {
+		btnOne.setText(actors.get(0).getName());
+		btnOne.repaint();
+		btnTwo.setText(actors.get(1).getName());
+		btnTwo.repaint();
+		btnThree.setText(actors.get(2).getName());
+		btnThree.repaint();
+		btnFour.setText(actors.get(3).getName());
+		btnFour.repaint();
+		btnFive.setText(actors.get(4).getName());
+		btnFive.repaint();
+		textPane3.setText(primaryActor.getName());
+		textPane3.repaint();
+		
+		URL url = null;
+		try {
+			if (primaryActorImage.size() > 0) {
+			 url = new URL("https://image.tmdb.org/t/p/original" 
+				+ primaryActorImage.get(0).getFilePath());
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (url != null) {
+			icon = new ImageIcon(url);
+			
+			Image img = icon.getImage();  
+			Image newimg = img.getScaledInstance(
+				150, 200,  java.awt.Image.SCALE_SMOOTH);  
+			icon = new ImageIcon(newimg);
+			lblNewLabel.setIcon(icon);
+			lblNewLabel.repaint();
+		}
+		
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 * @throws MalformedURLException 
+	 */
+	private void initialize() throws MalformedURLException {	
+		update();
 		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 620, 300);
+		frame.setBounds(100, 100, 620, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setLayout(null);
+		frame.getContentPane().setLayout(null);
 		
-		if (correctAnswer == 0) {
-			actor = relatedActor;
-		} else {
-			actor = TmDBModel.getIncorrectAnswers(
-					tmdbPeople, primaryActor);
-		}
-		JButton btnOne = new JButton(actor.getName());
+		btnOne = new JButton(actors.get(0).getName());
 		btnOne.setFont(new Font("Lucida Grande", Font.PLAIN, 9));
 		btnOne.addActionListener(new ActionListener() {
 			public void actionPerformed(final ActionEvent e) {
 				if (correctAnswer == 0) {
-					JOptionPane.showMessageDialog(null, "Correct Answer", "", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(
+						null, "Correct Answer", "",
+					   JOptionPane.INFORMATION_MESSAGE);
 				} else {
-					JOptionPane.showMessageDialog(null, "Incorrect Answer", "", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(
+						null, "Incorrect Answer", "",
+				       JOptionPane.INFORMATION_MESSAGE);
 				}
 			}
 		});
-		btnOne.setBounds(20, 175, 100, 50);
-		panel.add(btnOne);
+		btnOne.setBounds(20, 275, 100, 50);
+		frame.getContentPane().add(btnOne);
 		
-		if (correctAnswer == 1) {
-			actor = relatedActor;
-		} else {
-			actor = TmDBModel.getIncorrectAnswers(
-					tmdbPeople, primaryActor);
-		}
-		JButton btnTwo = new JButton(actor.getName());
+		btnTwo = new JButton(actors.get(1).getName());
 		btnTwo.setFont(new Font("Lucida Grande", Font.PLAIN, 9));
 		btnTwo.addActionListener(new ActionListener() {
 			public void actionPerformed(final ActionEvent e) {
 				if (correctAnswer == 1) {
-					JOptionPane.showMessageDialog(null, "Correct Answer", "", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(
+						null, "Correct Answer", "",
+					   JOptionPane.INFORMATION_MESSAGE);
 				} else {
-					JOptionPane.showMessageDialog(null, "Incorrect Answer", "", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(
+						null, "Incorrect Answer", "",
+					   JOptionPane.INFORMATION_MESSAGE);
 				}
 			}
 		});
-		btnTwo.setBounds(140, 175, 100, 50);
-		panel.add(btnTwo);
+		btnTwo.setBounds(140, 275, 100, 50);
+		frame.getContentPane().add(btnTwo);
 		
-		if (correctAnswer == 2) {
-			actor = relatedActor;
-		} else {
-			actor = TmDBModel.getIncorrectAnswers(
-					tmdbPeople, primaryActor);
-		}
-		JButton btnThree = new JButton(actor.getName());
+		btnThree = new JButton(actors.get(2).getName());
 		btnThree.setFont(new Font("Lucida Grande", Font.PLAIN, 9));
 		btnThree.addActionListener(new ActionListener() {
 			public void actionPerformed(final ActionEvent e) {
 				if (correctAnswer == 2) {
-					JOptionPane.showMessageDialog(null, "Correct Answer", "", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(
+						null, "Correct Answer", "",
+					   JOptionPane.INFORMATION_MESSAGE);
 				} else {
-					JOptionPane.showMessageDialog(null, "Incorrect Answer", "", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(
+						null, "Incorrect Answer", "",
+					   JOptionPane.INFORMATION_MESSAGE);
 				}
 			}
 		});
-		btnThree.setBounds(260, 175, 100, 50);
-		panel.add(btnThree);
+		btnThree.setBounds(260, 275, 100, 50);
+		frame.getContentPane().add(btnThree);
 		
-		if (correctAnswer == 3) {
-			actor = relatedActor;
-		} else {
-			actor = TmDBModel.getIncorrectAnswers(
-					tmdbPeople, primaryActor);
-		}
-		JButton btnFour = new JButton(actor.getName());
+		btnFour = new JButton(actors.get(3).getName());
 		btnFour.setFont(new Font("Lucida Grande", Font.PLAIN, 9));
 		btnFour.addActionListener(new ActionListener() {
 			public void actionPerformed(final ActionEvent e) {
 				if (correctAnswer == 3) {
-					JOptionPane.showMessageDialog(null, "Correct Answer", "", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(
+						null, "Correct Answer", "",
+					   JOptionPane.INFORMATION_MESSAGE);
 				} else {
-					JOptionPane.showMessageDialog(null, "Incorrect Answer", "", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(
+						null, "Incorrect Answer", "",
+					   JOptionPane.INFORMATION_MESSAGE);
 				}
 			}
 		});
-		btnFour.setBounds(380, 175, 100, 50);
-		panel.add(btnFour);
+		btnFour.setBounds(380, 275, 100, 50);
+		frame.getContentPane().add(btnFour);
 		
-		if (correctAnswer == 4) {
-			actor = relatedActor;
-		} else {
-			actor = TmDBModel.getIncorrectAnswers(
-					tmdbPeople, primaryActor);
-		}
-		JButton btnFive = new JButton(actor.getName());
+		btnFive = new JButton(actors.get(4).getName());
 		btnFive.setFont(new Font("Lucida Grande", Font.PLAIN, 9));
 		btnFive.addActionListener(new ActionListener() {
 			public void actionPerformed(final ActionEvent e) {
 				if (correctAnswer == 4) {
-					JOptionPane.showMessageDialog(null, "Correct Answer", "", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(
+						null, "Correct Answer", "",
+					   JOptionPane.INFORMATION_MESSAGE);
 				} else {
-					JOptionPane.showMessageDialog(null, "Incorrect Answer", "", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(
+						null, "Incorrect Answer", "",
+					   JOptionPane.INFORMATION_MESSAGE);
 				}
 			}
 		});
-		btnFive.setBounds(500, 175, 100, 50);
-		panel.add(btnFive);
+		btnFive.setBounds(500, 275, 100, 50);
+		frame.getContentPane().add(btnFive);
 		
-		JTextPane textPane3 = new JTextPane();
+		textPane3 = new JTextPane();
 		textPane3.setText(primaryActor.getName());
-		textPane3.setBackground(UIManager.getColor("Button.background"));
+		textPane3.setBackground(
+				UIManager.getColor("Button.background"));
 		textPane3.setBounds(310, 31, 259, 26);
-		panel.add(textPane3);
+		frame.getContentPane().add(textPane3);
 		
 		JTextPane txtpnWhichActorListed = new JTextPane();
 		txtpnWhichActorListed.setText(
 				"Which actor listed below is in a movie with:");
-		txtpnWhichActorListed.setBackground(UIManager.getColor("Button.background"));
+		txtpnWhichActorListed.setBackground(
+				UIManager.getColor("Button.background"));
 		txtpnWhichActorListed.setBounds(16, 31, 282, 16);
-		panel.add(txtpnWhichActorListed);
+		frame.getContentPane().add(txtpnWhichActorListed);
+		
+		updateGUI();
+
+		lblNewLabel = new JLabel("");
+
+		lblNewLabel.setIcon(icon);
+		lblNewLabel.setBounds(235, 60, 150, 200);
+		frame.getContentPane().add(lblNewLabel);
 		
 		JSplitPane splitPane = new JSplitPane();
-		frame.getContentPane().add(splitPane, BorderLayout.SOUTH);
+		splitPane.setBounds(10, 340, 600, 33);
+		frame.getContentPane().add(splitPane);
+		splitPane.setResizeWeight(0.3);
 		
 		JButton btnQuit = new JButton("Quit");
 		btnQuit.addActionListener(new ActionListener() {
 			public void actionPerformed(final ActionEvent e) {
+				System.exit(1);
 			}
 		});
 		splitPane.setLeftComponent(btnQuit);
 		
-		JButton btnPlay = new JButton("Play");
+		JButton btnPlay = new JButton("New Question");
+		btnPlay.addActionListener(new ActionListener() {
+			public void actionPerformed(final ActionEvent e) {
+				update();
+				updateGUI();
+			}
+		});
 		splitPane.setRightComponent(btnPlay);
 	}
 }
--- src/edu/gvsu/cis350/TmDBModel.java
+++ src/edu/gvsu/cis350/TmDBModel.java
@@ -37,244 +37,51 @@ public final class TmDBModel {
 	 */
 	private static TmdbApi tmdbApi 
 		= new TmdbApi("ee5a0a6208f35c4a8010636efd3f5d9b");
-	/**
-	 * 
-	 */
-	private static SessionToken sessionToken = getSessionToken();
+
 	
 	/**
-	 * @return SessionToken
+	 * @param tmdbPeople TmdbPeople
+	 * @return Person
 	 */
-	private static SessionToken getSessionToken() {
-		// There are two ways to generate a session id
-		
-		/* Method 1: Generating session id using API calls
-			(requires username and password)
-		*/
-		
-		
-		/*TmdbAuthentication tmdbAuth = tmdbApi.getAuthentication();
-		TokenSession tokenSession 
-			= tmdbAuth.getSessionLogin("kielstreak","Password");
-		System.out.println("Session ID: " 
-			+ tokenSession.getSessionId());
-		SessionToken sessionToken 
-			= new SessionToken(tokenSession.getSessionId());
-		*/
-		
-		// Method 2: Generating session id via the website
-		// Step 1: create a new request token
-		//		http://api.themoviedb.org/3/authentication
-		//				/token/new?api_key=your-api-key
-		//		(note down the request_token from the response)
-		// Step 2: ask the user for permission
-		//		https://www.themoviedb.org/authenticate
-		//				/request_token
-		// Step 3: create a session id
-		//		http://api.themoviedb.org/3/authentication
-		//			/session/new?api_key=api-key
-		//			&request_token=request-token
-		//	    (use session-id value in the response to set the 
-		//		value for sessionId variable in the code below)
-		
-		// hard-coded session id generated from Method 1 above
-		SessionToken sessionToken 
-			= new SessionToken(
-				"8d9dc5390a6fcf433d7406c0c47453b8ce23fd4c");
-		return sessionToken;
-	}
-	
-	/**
-	 * 
-	 */
-	public static void demoMovies() {
-		TmdbMovies tmdbMovies = tmdbApi.getMovies();
-		
-		// top rated movies
-		MovieResultsPage results 
-			= tmdbMovies.getTopRatedMovies("en", 0);
-		
-		System.out.println("======= Top rated movies ============");
-		Iterator<MovieDb> iterator = results.iterator();
-		while (iterator.hasNext()) {
-			MovieDb movie = iterator.next();
-			System.out.println(movie.getTitle());
-			System.out.println(movie.getOriginalTitle());
-			System.out.println(movie.getReleaseDate());
-			System.out.println();
-		}
-		System.out.println("======= Top rated movies ============");
-	}
-	
-	/**
-	 * 
-	 */
-	public static void demoMovieFeatures() {
-		TmdbMovies tmdbMovies = tmdbApi.getMovies();
-		
-		System.out.println("======= Movies features ============");
-		
-		// retrieve information on a movie given movie id
-		MovieDb movie = tmdbMovies.getMovie(271110, "en", 
-				MovieMethod.credits, MovieMethod.reviews, 
-				MovieMethod.videos);
-		System.out.println(movie.getTitle());
-		System.out.println(movie.getReleaseDate());
-		System.out.println(movie.getRuntime());
-		System.out.println(movie.getOverview());
-		System.out.println();
-		
-		// retrieve information on movie cast
-		List<PersonCast> cast = movie.getCast();
-		if (cast == null) {
-			System.out.println(
-				"Cast info not available for this movie");
-		} else {
-			Iterator<PersonCast> iterator = cast.iterator();
-			while (iterator.hasNext()) {
-				PersonCast person = iterator.next();
-				System.out.println(person.getName());
-				System.out.println(person.getCharacter());
-				System.out.println();
-			}
-		}
-		
-		// retrieve information on movie crew
-		List<PersonCrew> crew = movie.getCrew();
-		if (crew == null) {
-			System.out.println(
-				"Crew info not available for this movie");
-		} else {
-			Iterator<PersonCrew> iterator = crew.iterator();
-			while (iterator.hasNext()) {
-				PersonCrew person = iterator.next();
-				System.out.println(person.getName());
-				System.out.println(person.getJob());
-				System.out.println();
-			}
-		}
-		
-		// get review for the movie
-		List<Reviews> reviews = movie.getReviews();
-		if (reviews == null) {
-			System.out.println(
-				"Reviews not available for this movie");
-		} else {
-			Iterator<Reviews> iterator = reviews.iterator();
-			while (iterator.hasNext()) {
-				Reviews review = iterator.next();
-				System.out.println(review.getAuthor());
-				System.out.println(review.getContent());
-				System.out.println();
-			}
-		}
-		
-		// retrieve information on movie videos
-		List<Video> videos = movie.getVideos();
-		if (videos == null) {
-			System.out.println(
-				"Videos info not available for this movie");
-		} else {
-			Iterator<Video> iterator = videos.iterator();
-			while (iterator.hasNext()) {
-				Video video = iterator.next();
-				System.out.println(video.getSite());
-				System.out.println(video.getKey());
-				System.out.println(video.getName());
-				System.out.println(video.getType());
-				System.out.println(video.getSize());
-				System.out.println();
-			}
-		}
-		
-		System.out.println("======= Movies features ============");
-	}
-	
-	/**
-	 * 
-	 */
-	public static void demoSearchFeatures() {
-		TmdbSearch tmdbSearch = tmdbApi.getSearch();
-		
-		// search for movies containing "civil war" in title
-		MovieResultsPage results 
-		    = tmdbSearch.searchMovie("civil war", 0, "en", false, 0);
-		Iterator<MovieDb> iterator = results.iterator();
-		while (iterator.hasNext()) {
-			MovieDb movie = iterator.next();
-			System.out.println(movie.getTitle());
-			System.out.println(movie.getOriginalTitle());
-			System.out.println(movie.getReleaseDate());
-			System.out.println();
-		}	
-	}
-	
-	/**
-	 * 
-	 */
-	public static void demoAccountFeatures() {
-		TmdbAccount tmdbAccount = tmdbApi.getAccount();
-		
-		// get basic account information
-		Account act = tmdbAccount.getAccount(sessionToken);
-		System.out.println(act.getId());
-		System.out.println(act.getUserName());
-		System.out.println(act.getName());
-		System.out.println();
-		
-		// get favorite movies of the account
-		AccountID actId = new AccountID(act.getId());
-		MovieResultsPage results 
-			= tmdbAccount.getFavoriteMovies(sessionToken, actId);
-		System.out.println(
-			"Movies in the favorite list: " 
-			+ results.getTotalResults());
-		Iterator<MovieDb> iterator = results.iterator();
-		while (iterator.hasNext()) {
-			MovieDb movie = iterator.next();
-			System.out.println(movie.getTitle());
-			System.out.println(movie.getReleaseDate());
-			System.out.println();
-		}
-		
-		// get movies in the watch list of the account
-		results 
-		   = tmdbAccount.getWatchListMovies(sessionToken, actId, 0);
-		System.out.println(
-			"Movies in the watch list: " 
-			+ results.getTotalResults());
-		iterator = results.iterator();
-		while (iterator.hasNext()) {
-			MovieDb movie = iterator.next();
-			System.out.println(movie.getTitle());
-			System.out.println(movie.getId());
-			System.out.println(movie.getReleaseDate());
-			System.out.println();
-		}	
-	}
-	
-	public static Person getPrimaryActor(TmdbPeople tmdbPeople) {
-		List<Person> popularList = tmdbPeople.getPersonPopular(0).getResults();
+	public static Person getPrimaryActor(final TmdbPeople tmdbPeople) {
+		List<Person> popularList 
+			= tmdbPeople.getPersonPopular(0).getResults();
 		Random randomIndex = new Random();
 		return popularList.get(randomIndex.nextInt(popularList.size()));
 	}
 	
-	public static Person getIncorrectAnswers(TmdbPeople tmdbPeople, Person primaryActor){
+	/**
+	 * @param tmdbPeople TmdbPeople
+	 * @param primaryActor Person
+	 * @return Person
+	 */
+	public static Person getIncorrectAnswers(
+		   final TmdbPeople tmdbPeople, final Person primaryActor) {
 		boolean unrelatedActors = true;
-		PersonCredits primaryActorCredits = tmdbPeople.getPersonCredits(primaryActor.getId());
+		PersonCredits primaryActorCredits 
+			= tmdbPeople.getPersonCredits(primaryActor.getId());
 		Random randomIndex = new Random();
-		List<Person> popularList = tmdbPeople.getPersonPopular(randomIndex.nextInt(100)).getResults();
-		Person secondaryActor = popularList.get(randomIndex.nextInt(popularList.size()));
-		PersonCredits secondaryActorCredits = tmdbPeople.getPersonCredits(secondaryActor.getId());
-		Iterator<PersonCredit> primaryIterator = primaryActorCredits.getCast().iterator();
-		Iterator<PersonCredit> secondaryIterator = secondaryActorCredits.getCast().iterator();
-		while(primaryIterator.hasNext()){
+		List<Person> popularList 
+			= tmdbPeople.getPersonPopular(
+				randomIndex.nextInt(100)).getResults();
+		Person secondaryActor 
+			= popularList.get(randomIndex.nextInt(
+				popularList.size()));
+		PersonCredits secondaryActorCredits 
+			= tmdbPeople.getPersonCredits(secondaryActor.getId());
+		Iterator<PersonCredit> primaryIterator 
+			= primaryActorCredits.getCast().iterator();
+		Iterator<PersonCredit> secondaryIterator 
+			= secondaryActorCredits.getCast().iterator();
+		while (primaryIterator.hasNext()) {
 			PersonCredit primaryCredit = primaryIterator.next();
 			int primaryMovieID = primaryCredit.getMovieId();
-			while(secondaryIterator.hasNext()){
-				PersonCredit secondaryCredit = secondaryIterator.next();
-				int secondaryMovieID = secondaryCredit.getMovieId();
-				if(primaryMovieID == secondaryMovieID){
+			while (secondaryIterator.hasNext()) {
+				PersonCredit secondaryCredit 
+					= secondaryIterator.next();
+				int secondaryMovieID 
+					= secondaryCredit.getMovieId();
+				if (primaryMovieID == secondaryMovieID) {
 					unrelatedActors = false;
 					break;
 				}
@@ -290,15 +97,26 @@ public final class TmDBModel {
 		}
 	}
 	
-	public static PersonCast getRelatedActor(TmdbPeople tmdbPeople,TmdbMovies tmdbMovies, Person primaryActor){
+	/**
+	 * @param tmdbPeople TmdbPeople
+	 * @param tmdbMovies TmdbMovies
+	 * @param primaryActor Person
+	 * @return PersonCast
+	 */
+	public static PersonCast getRelatedActor(final TmdbPeople tmdbPeople, 
+		   final TmdbMovies tmdbMovies, final Person primaryActor) {
 		int actorId = primaryActor.getId(); 
-        List<PersonCredit> movieList = tmdbPeople.getPersonCredits(actorId).getCast();
+        List<PersonCredit> movieList 
+        	= tmdbPeople.getPersonCredits(actorId).getCast();
         Random randomIndex = new Random();
-        int randomMovieId = movieList.get(randomIndex.nextInt(movieList.size())).getMovieId();
+        int randomMovieId 
+        	= movieList.get(randomIndex.nextInt(
+        			movieList.size())).getMovieId();
         System.out.println(tmdbMovies.getMovie(randomMovieId, "en"));
         List<PersonCast> cast = tmdbMovies.getCredits(randomMovieId).getCast();
        
-        PersonCast randomRelatedPerson = cast.get(randomIndex.nextInt(cast.size()));
+        PersonCast randomRelatedPerson 
+        	= cast.get(randomIndex.nextInt(cast.size()));
        
         System.out.println(randomRelatedPerson.getName());
         return randomRelatedPerson;
