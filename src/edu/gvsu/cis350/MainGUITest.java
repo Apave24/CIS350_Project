a34ef8b78701d1e73d0e35ff319d305a3e142795
Alex Pavey <paveya@mail.gvsu.edu>
2017-07-19 20:13:23 -0400

    Add JUnit tests

--- src/edu/gvsu/cis350/MainGUITest.java
+++ src/edu/gvsu/cis350/MainGUITest.java
@@ -1,11 +1,10 @@
 package edu.gvsu.cis350;
 
-import static org.junit.Assert.fail;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * @author evankiel
+ * @author Alex Pavey
  *
  */
 public class MainGUITest {
@@ -14,8 +13,39 @@ public class MainGUITest {
 	 * 
 	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testUpdateGUI() {
+		try{
+			MainGUI testGUI = new MainGUI();
+			
+			/** Cannot test because these fields are private
+			String previousTextPaneValue = testGUI.textPane.getText();
+			String previousTextPane1Value = testGUI.textPane.getText();
+			String previousTextPane2Value = testGUI.textPane.getText();
+			String previousTextPane3Value = testGUI.textPane.getText();
+			String previousTextPane4Value = testGUI.textPane.getText();
+			**/
+			
+			testGUI.update();
+			testGUI.updateGUI();
+			
+			/** Cannot test because these fields are private
+			String newTextPaneValue = testGUI.textPane.getText();
+			String newTextPane1Value = testGUI.textPane.getText();
+			String newTextPane2Value = testGUI.textPane.getText();
+			String newTextPane3Value = testGUI.textPane.getText();
+			String newTextPane4Value = testGUI.textPane.getText();
+			
+			Assert.assertNotEquals(previousTextPaneValue, newTextPaneValue);
+			Assert.assertNotEquals(previousTextPane1Value, newTextPane1Value);
+			Assert.assertNotEquals(previousTextPane2Value, newTextPane2Value);
+			Assert.assertNotEquals(previousTextPane3Value, newTextPane3Value);
+			Assert.assertNotEquals(previousTextPane4Value, newTextPane4Value);
+			**/
+			
+		} catch (Exception e) {
+			Assert.fail("GUI failed to initialize.");
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/edu/gvsu/cis350/TmDBModelTest.java
+++ src/edu/gvsu/cis350/TmDBModelTest.java
@@ -1,16 +1,21 @@
 package edu.gvsu.cis350;
 
 
-import static org.junit.Assert.assertEquals;
-import info.movito.themoviedbapi.TmdbApi;
-//import info.movito.themoviedbapi.TmdbMovies;
-import info.movito.themoviedbapi.TmdbPeople;
-import info.movito.themoviedbapi.model.people.Person;
+import java.util.Iterator;
+import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import info.movito.themoviedbapi.TmdbApi;
+import info.movito.themoviedbapi.TmdbPeople;
+import info.movito.themoviedbapi.TmdbMovies;
+import info.movito.themoviedbapi.model.people.Person;
+import info.movito.themoviedbapi.model.people.PersonCast;
+import info.movito.themoviedbapi.model.people.PersonCredit;
+
 /**
- * @author evankiel
+ * @author Alex Pavey
  *
  */
 public class TmDBModelTest {
@@ -18,26 +23,70 @@ public class TmDBModelTest {
 	/**
 	 * 
 	 */
-	private TmdbApi tmdbApi 
-		= new TmdbApi("ee5a0a6208f35c4a8010636efd3f5d9b");
-	/**
-	 * 
-	 */
+	private TmdbApi tmdbApi = new TmdbApi("ee5a0a6208f35c4a8010636efd3f5d9b");
 	private TmdbPeople tmdbPeople = tmdbApi.getPeople();
-	/**
-	 * 
-	 */
-	//private TmdbMovies tmdbMovies = tmdbApi.getMovies(); 
+	private TmdbMovies tmdbMovies = tmdbApi.getMovies(); 
 
 	/**
 	 * 
 	 */
 	@Test
+	public void testPrimaryActor() {
+		Person primaryActor = null;
+		primaryActor = TmDBModel.getPrimaryActor(tmdbPeople);
+		Assert.assertNotNull(primaryActor);
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
 	public void testIncorrectActor() {
+		boolean actorInMovieList = false;
 		Person primaryActor = TmDBModel.getPrimaryActor(tmdbPeople);
-		assertEquals(false, 
-			TmDBModel.getIncorrectAnswers(tmdbPeople, primaryActor)
-			.getCastId() == primaryActor.getCastId());
+		Person unrelatedActor = TmDBModel.getIncorrectAnswers(tmdbPeople, primaryActor);
+		
+		List<PersonCredit> actorMovies = tmdbPeople.getPersonCredits(primaryActor.getId()).getCast();
+		Iterator<PersonCredit> iterator = actorMovies.iterator();
+		while (iterator.hasNext()) {
+			PersonCredit movie = iterator.next();
+			List<PersonCast> cast = tmdbMovies.getCredits(movie.getMovieId()).getCast();
+			Iterator<PersonCast> relatedActors = cast.iterator();
+			while(relatedActors.hasNext()){
+				PersonCast actor = relatedActors.next();
+				if(actor.getId() == unrelatedActor.getId()){
+					actorInMovieList = true;
+				}
+			}
+		}
+		
+		Assert.assertEquals(false, actorInMovieList);
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testMatchingActor() {
+		boolean actorInMovieList = false;
+		Person primaryActor = TmDBModel.getPrimaryActor(tmdbPeople);
+		Person matchingActor = TmDBModel.getRelatedActor(tmdbPeople, tmdbMovies, primaryActor);
+		
+		List<PersonCredit> actorMovies = tmdbPeople.getPersonCredits(primaryActor.getId()).getCast();
+		Iterator<PersonCredit> iterator = actorMovies.iterator();
+		while (iterator.hasNext()) {
+			PersonCredit movie = iterator.next();
+			List<PersonCast> cast = tmdbMovies.getCredits(movie.getMovieId()).getCast();
+			Iterator<PersonCast> relatedActors = cast.iterator();
+			while(relatedActors.hasNext()){
+				PersonCast actor = relatedActors.next();
+				if(actor.getId() == matchingActor.getId()){
+					actorInMovieList = true;
+				}
+			}
+		}
+		
+		Assert.assertEquals(true, actorInMovieList);
 	}
 
 }
