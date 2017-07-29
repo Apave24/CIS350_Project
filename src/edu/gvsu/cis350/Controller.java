e6d1629ab7dcfe380108905f664e9d1a5279151d
Alex Pavey <paveya@mail.gvsu.edu>
2017-07-29 19:09:44 -0400

    Add Six Degrees functionality and update gui.

--- src/edu/gvsu/cis350/Controller.java
+++ src/edu/gvsu/cis350/Controller.java
@@ -6,13 +6,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.image.Image;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.ResourceBundle;
 
 import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbMovies;
@@ -37,34 +36,32 @@ public class Controller{
 	private int correctAnswer;
 	
 	/**
-	 * Image selected for the primary actor.
+	 * Stage of the game
 	 */
-	private List<Artwork> pAImage;
+	private int degreeCounter = 1;
+	
+	/**
+	 * Image list for the primary actor.
+	 */
+	private List<Artwork> pAImageList;
+	
+	private List<Artwork> Opt1ImageList;
+	private List<Artwork> Opt2ImageList;
+	private List<Artwork> Opt3ImageList;
+	private List<Artwork> Opt4ImageList;
+	private List<Artwork> Opt5ImageList;
 	
 	/**
 	 * Image selected for the primary actor.
 	 */
-	private List<Artwork> Opt1Image;
+	private Image pAImage;
 	
-	/**
-	 * Image selected for the primary actor.
-	 */
-	private List<Artwork> Opt2Image;
+	private Image Opt1Image;
+	private Image Opt2Image;
+	private Image Opt3Image;
+	private Image Opt4Image;
+	private Image Opt5Image;
 	
-	/**
-	 * Image selected for the primary actor.
-	 */
-	private List<Artwork> Opt3Image;
-	
-	/**
-	 * Image selected for the primary actor.
-	 */
-	private List<Artwork> Opt4Image;
-	
-	/**
-	 * Image selected for the primary actor.
-	 */
-	private List<Artwork> Opt5Image;
 	/**
 	 * The actor chosen to compare to.
 	 */
@@ -78,7 +75,9 @@ public class Controller{
 	/**
 	 * Number of correct answers.
 	 */
-	private int wins = -1;
+	private int wins = 0;
+	
+	private Artwork noArtworkAvailable;
 	
 	@FXML private ImageView PrimaryActorImage;
 	@FXML private Label PrimaryActorName;
@@ -95,27 +94,52 @@ public class Controller{
 	@FXML private Label OptionName4;
 	@FXML private Label OptionName5;
 	
+	@FXML private ImageView FirstDegreeImg;
+	@FXML private ImageView SecondDegreeImg;
+	@FXML private ImageView ThirdDegreeImg;
+	@FXML private ImageView FourthDegreeImg;
+	@FXML private ImageView FifthDegreeImg;
+	@FXML private ImageView SixthDegreeImg;
+	
+	@FXML private Label FirstDegreeName;
+	@FXML private Label SecondDegreeName;
+	@FXML private Label ThirdDegreeName;
+	@FXML private Label FourthDegreeName;
+	@FXML private Label FifthDegreeName;
+	@FXML private Label SixthDegreeName;
+	
+	@FXML private Label UserScore;
+	@FXML private Label AnswerStatus;
+	
 	public Controller(){
 		this.tmdbApi = new TmdbApi("ee5a0a6208f35c4a8010636efd3f5d9b");
 	    this.tmdbPeople = tmdbApi.getPeople();
 		this.tmdbMovies = tmdbApi.getMovies();
+		
+		this.noArtworkAvailable = new Artwork();
+		this.noArtworkAvailable.setFilePath("file://" + getClass().getResource("NoPhoto.jpg").getPath());
 	}
 	
 	@FXML 
 	public void initialize(){
+		primaryActor = TmDBModel.getPrimaryActor(tmdbPeople);
+		
 		updateData();
 		updateGUI();
+		addDegreePhoto(pAImage);
 	}
 	
 	/**
 	 * Updates the question and answers for the game.
 	 */
 	public void updateData() {
-		wins++;
+		
+		if(degreeCounter != 1){
+			primaryActor = actors.get(correctAnswer);
+		}
+		
 		Random randomIndex = new Random();
 		correctAnswer = randomIndex.nextInt(5);
-		
-		primaryActor = TmDBModel.getPrimaryActor(tmdbPeople);
 		
 		do {
 			relatedActor = TmDBModel.getRelatedActor(
@@ -134,12 +158,55 @@ public class Controller{
 		}
 		
 		// Update image data
-		pAImage = tmdbPeople.getPersonImages(primaryActor.getId());
-		Opt1Image = tmdbPeople.getPersonImages(actors.get(0).getId());
-		Opt2Image = tmdbPeople.getPersonImages(actors.get(1).getId());
-		Opt3Image = tmdbPeople.getPersonImages(actors.get(2).getId());
-		Opt4Image = tmdbPeople.getPersonImages(actors.get(3).getId());
-		Opt5Image = tmdbPeople.getPersonImages(actors.get(4).getId());
+		pAImageList = tmdbPeople.getPersonImages(primaryActor.getId());
+		if(pAImageList.isEmpty()){
+			pAImage = new Image(noArtworkAvailable.getFilePath());
+		}
+		else {
+			pAImage = new Image("https://image.tmdb.org/t/p/original" + pAImageList.get(0).getFilePath());
+		}
+		
+		Opt1ImageList = tmdbPeople.getPersonImages(actors.get(0).getId());
+		System.out.println(noArtworkAvailable.getFilePath());
+		if(Opt1ImageList.isEmpty()){
+			Opt1Image = new Image(noArtworkAvailable.getFilePath());
+		}
+		else {
+			Opt1Image = new Image("https://image.tmdb.org/t/p/original" + Opt1ImageList.get(0).getFilePath());
+		}
+		
+		Opt2ImageList = tmdbPeople.getPersonImages(actors.get(1).getId());
+		if(Opt2ImageList.isEmpty()){
+			Opt2Image = new Image(noArtworkAvailable.getFilePath());
+		}
+		else {
+			Opt2Image = new Image("https://image.tmdb.org/t/p/original" + Opt2ImageList.get(0).getFilePath());
+		}
+		
+		Opt3ImageList = tmdbPeople.getPersonImages(actors.get(2).getId());
+		if(Opt3ImageList.isEmpty()){
+			Opt3Image = new Image(noArtworkAvailable.getFilePath());
+		}
+		else {
+			Opt3Image = new Image("https://image.tmdb.org/t/p/original" + Opt3ImageList.get(0).getFilePath());
+		}
+		
+		Opt4ImageList = tmdbPeople.getPersonImages(actors.get(3).getId());
+		if(Opt4ImageList.isEmpty()){
+			Opt4Image = new Image(noArtworkAvailable.getFilePath());
+		}
+		else {
+			Opt4Image = new Image("https://image.tmdb.org/t/p/original" + Opt4ImageList.get(0).getFilePath());
+		}
+		
+		Opt5ImageList = tmdbPeople.getPersonImages(actors.get(4).getId());
+		if(Opt5ImageList.isEmpty()){
+			Opt5Image = new Image(noArtworkAvailable.getFilePath());
+		}
+		else {
+			Opt5Image = new Image("https://image.tmdb.org/t/p/original" + Opt5ImageList.get(0).getFilePath());
+		}
+		
 	}
 	
 	/**
@@ -148,12 +215,11 @@ public class Controller{
 	public void updateGUI() {
 		
 		// Update ImageView images
-		PrimaryActorImage.setImage(new Image("https://image.tmdb.org/t/p/original" + pAImage.get(0).getFilePath()));
-		OptionImage1.setImage(new Image("https://image.tmdb.org/t/p/original" + Opt1Image.get(0).getFilePath()));
-		OptionImage2.setImage(new Image("https://image.tmdb.org/t/p/original" + Opt2Image.get(0).getFilePath()));
-		OptionImage3.setImage(new Image("https://image.tmdb.org/t/p/original" + Opt3Image.get(0).getFilePath()));
-		OptionImage4.setImage(new Image("https://image.tmdb.org/t/p/original" + Opt4Image.get(0).getFilePath()));
-		OptionImage5.setImage(new Image("https://image.tmdb.org/t/p/original" + Opt5Image.get(0).getFilePath()));
+		OptionImage1.setImage(Opt1Image);
+		OptionImage2.setImage(Opt2Image);
+		OptionImage3.setImage(Opt3Image);
+		OptionImage4.setImage(Opt4Image);
+		OptionImage5.setImage(Opt5Image);
 		
 		// Update Label text
 		PrimaryActorName.setText(primaryActor.getName());
@@ -163,30 +229,136 @@ public class Controller{
 		OptionName4.setText(actors.get(3).getName());
 		OptionName5.setText(actors.get(4).getName());
 	}
+	
+	private void addDegreePhoto(Image correctImage){
+		switch(degreeCounter){
+		case 1:
+			FirstDegreeImg.setImage(correctImage);
+			FirstDegreeName.setText(actors.get(correctAnswer).getName());
+			break;
+		case 2:
+			SecondDegreeImg.setImage(correctImage);
+			SecondDegreeName.setText(actors.get(correctAnswer).getName());
+			break;
+		case 3:
+			ThirdDegreeImg.setImage(correctImage);
+			ThirdDegreeName.setText(actors.get(correctAnswer).getName());
+			break;
+		case 4:
+			FourthDegreeImg.setImage(correctImage);
+			FourthDegreeName.setText(actors.get(correctAnswer).getName());
+			break;
+		case 5:
+			FifthDegreeImg.setImage(correctImage);
+			FifthDegreeName.setText(actors.get(correctAnswer).getName());
+			break;
+		case 6:
+			SixthDegreeImg.setImage(correctImage);
+			SixthDegreeName.setText(actors.get(correctAnswer).getName());
+			break;
+		}
+		
+		degreeCounter++;
+	}
 
     @FXML protected void handleOption1Click(MouseEvent me) {
-    	//TODO
-    	//checkCorrectAnswer()
+    	if(correctAnswer == 0){
+    		wins++;
+    		AnswerStatus.setText("Correct!");
+    		AnswerStatus.setTextFill(Color.web("#008000"));
+    		if(me.getSource() instanceof ImageView){
+    			ImageView clickedImage = (ImageView) me.getSource();
+    			addDegreePhoto(clickedImage.getImage());
+    		}
+    		updateData();
+        	updateGUI();
+    	}
+    	else {
+    		wins--;
+    		AnswerStatus.setText("Incorrect!");
+    		AnswerStatus.setTextFill(Color.web("#FF0000"));
+    	}
+    	UserScore.setText(new Integer(wins).toString());
     }
     
     @FXML protected void handleOption2Click(MouseEvent me) {
-    	//TODO
-    	//checkCorrectAnswer()
+    	if(correctAnswer == 1){
+    		wins++;
+    		AnswerStatus.setText("Correct!");
+    		AnswerStatus.setTextFill(Color.web("#008000"));
+    		if(me.getSource() instanceof ImageView){
+    			ImageView clickedImage = (ImageView) me.getSource();
+    			addDegreePhoto(clickedImage.getImage());
+    		}
+    		updateData();
+        	updateGUI();
+    	}
+    	else {
+    		wins--;
+    		AnswerStatus.setText("Incorrect!");
+    		AnswerStatus.setTextFill(Color.web("#FF0000"));
+    	}
+    	UserScore.setText(new Integer(wins).toString());
     }
 
     @FXML protected void handleOption3Click(MouseEvent me) {
-    	//TODO
-    	//checkCorrectAnswer()
+    	if(correctAnswer == 2){
+    		wins++;
+    		AnswerStatus.setText("Correct!");
+    		AnswerStatus.setTextFill(Color.web("#008000"));
+    		if(me.getSource() instanceof ImageView){
+    			ImageView clickedImage = (ImageView) me.getSource();
+    			addDegreePhoto(clickedImage.getImage());
+    		}
+    		updateData();
+        	updateGUI();
+    	}
+    	else {
+    		wins--;
+    		AnswerStatus.setText("Incorrect!");
+    		AnswerStatus.setTextFill(Color.web("#FF0000"));
+    	}
+    	UserScore.setText(new Integer(wins).toString());
     }
     
     @FXML protected void handleOption4Click(MouseEvent me) {
-    	//TODO
-    	//checkCorrectAnswer()
+    	if(correctAnswer == 3){
+    		wins++;
+    		AnswerStatus.setText("Correct!");
+    		AnswerStatus.setTextFill(Color.web("#008000"));
+    		if(me.getSource() instanceof ImageView){
+    			ImageView clickedImage = (ImageView) me.getSource();
+    			addDegreePhoto(clickedImage.getImage());
+    		}
+    		updateData();
+        	updateGUI();
+    	}
+    	else {
+    		wins--;
+    		AnswerStatus.setText("Incorrect!");
+    		AnswerStatus.setTextFill(Color.web("#FF0000"));
+    	}
+    	UserScore.setText(new Integer(wins).toString());
     }
     
     @FXML protected void handleOption5Click(MouseEvent me) {
-    	//TODO
-    	//checkCorrectAnswer()
+    	if(correctAnswer == 4){
+    		wins++;
+    		AnswerStatus.setText("Correct!");
+    		AnswerStatus.setTextFill(Color.web("#008000"));
+    		if(me.getSource() instanceof ImageView){
+    			ImageView clickedImage = (ImageView) me.getSource();
+    			addDegreePhoto(clickedImage.getImage());
+    		}
+    		updateData();
+        	updateGUI();
+    	}
+    	else {
+    		wins--;
+    		AnswerStatus.setText("Incorrect!");
+    		AnswerStatus.setTextFill(Color.web("#FF0000"));
+    	}
+    	UserScore.setText(new Integer(wins).toString());
     }
     
     @FXML protected void handleSkipQuestion(ActionEvent event){
