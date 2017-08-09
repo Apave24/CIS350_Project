ba76e2a142f079e0b7f30447d041dd7dd810dd24
kielstreak <kielev@mail.gvsu.edu>
2017-08-08 22:57:09 -0400

    added ControllerTest and JavaDocs

---
+++ src/edu/gvsu/cis350/ControllerTest.java
@@ -0,0 +1,71 @@
+package edu.gvsu.cis350;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Controller JUnit testing.
+ * 
+ * @author Nicholas Borchardt
+ *
+ */
+public class ControllerTest {
+
+	/**
+	 * Initializes the GUI and makes sure it doesn't fail.
+	 */
+	@Test
+	public void initializeTest() {
+		try {
+			//Controller testController = new Controller();
+			
+		/** 
+		* Cannot test because all fields 
+		* in the Control Class are private.
+		**/
+			
+		} catch (Exception e) {
+			Assert.fail("GUI failed to initialize.");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Updates the data and makes sure it doesn't fail.
+	 */
+	@Test
+	public void updateDataTest() {
+		try {
+			//Controller testController = new Controller();
+			
+		/** 
+		 * Cannot test because all fields 
+		 * in the Control Class are private.
+		**/
+			
+		} catch (Exception e) {
+			Assert.fail("GUI failed to initialize.");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Updates the data and makes sure it doesn't fail.
+	 */
+	@Test
+	public void updateGUITest() {
+		try {
+			//Controller testController = new Controller();
+			
+		/** 
+		 * Cannot test because all fields 
+		 * in the Control Class are private.
+		**/
+			
+		} catch (Exception e) {
+			Assert.fail("GUI failed to initialize.");
+			e.printStackTrace();
+		}
+	}
+
+}
